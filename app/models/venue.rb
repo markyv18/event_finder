b2797066e1ea47baedff98d3cf42e212e6e1a91d
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-06-19 15:11:06 -0600

    building out API consumption parsing and display

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,25 @@
 class Venue < ActiveRecord::Base
   validates :name, uniqueness: true
 
+  def self.zip(zip)
+    events = VenueService.zip_search(zip)
+    events.map do |event|
+      Venue.new(event)
+    end
+  end
+
+end
+
+
+attr_reader
+
+def initialize(params)
+  @event = params[:symbol]
+
+    #event name
+    #date and time
+    #description (140 chars)
+    #venue name (hyperlinked to venue page)
+    #venue address
+
 end
--- app/services/venue_service.rb
+++ app/services/venue_service.rb
@@ -2,6 +2,6 @@ class VenueService
 
   def self.zip_search(zip)
     response = Faraday.get("")
-    stores = JSON.parse(response.body, symbolize_names: true)
+    events = JSON.parse(response.body, symbolize_names: true)
   end
 end
