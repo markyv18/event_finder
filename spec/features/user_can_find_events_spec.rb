d9d8d5eea937c32efdc951b13d42bdaaacd7663d
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-06-19 14:39:51 -0600

    building out test for API consumption

--- spec/features/user_can_find_events_spec.rb
+++ spec/features/user_can_find_events_spec.rb
@@ -5,13 +5,27 @@ describe "as a user" do
   it "i can enter a zipcode and find all events for that zip sorted by popularity " do
     visit '/'
 
-    fill_in 'zip', with: 
+    fill_in 'zip', with: "80202"
+
+    click_on "Find Events"
+
+    expect(current_path).to eq(search_path)
+    expect(current_path).to eq('/search')
+
+    # And I can see a list of 16 events for next month sorted by popularity
+    expect(page).to have_content("Number of Events: 16")
+
+    # And I can see a header `196 events for July`.
+    within('h3') do
+      expect(page).to have_content("196 events for July")
+    end
+
+    # For each event I can see...
+    expect(page).to have_content      #event name
+    expect(page).to have_content      #date and time
+    expect(page).to have_content      #description (140 chars)
+    expect(page).to have_content      #venue name (hyperlinked to venue page)
+    expect(page).to have_content      #venue address
 
   end
 end
-And I fill in the search box with 80202
-And I click Find Events
-The current path is `/search`
-And I can see a list of 16 events for next month sorted by popularity
-And I can see a header `196 events for July`.
-For each event I can see the Event name, date & time, description (shortened to 140 characters), venue name (url to the venue page), venue address.
