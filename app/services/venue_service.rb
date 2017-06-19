f8e1631eea21dc694ac924b355c57fa50347fd34
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-06-19 15:34:33 -0600

    still parsing API

--- app/services/venue_service.rb
+++ app/services/venue_service.rb
@@ -2,8 +2,8 @@ class VenueService
 
 #{"API_Key"}
   def self.zip_search(zip)
-    response = Faraday.get("http://api.eventful.com/json/events/search?app_key=2J7ttvFCd8S3W2vT&location=#{zip}")
-    events = JSON.parse(response.body, symbolize_names: true)
+    response = Faraday.get("http://api.eventful.com/json/events/search?app_key=2J7ttvFCd8S3W2vT&location=#{zip}&sort_order=popularity&page_size=16")
+    JSON.parse(response.body, symbolize_names: true)[:events][:event]
     require "pry"; binding.pry
   end
 end
