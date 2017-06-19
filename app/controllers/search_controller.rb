87b5420280eceb6705133e5f9650f05a182c69b7
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-06-19 16:33:22 -0600

    view up, moving to endpoints

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
   attr_reader :events
-  
+
   def index
     @events = Venue.zip(params[:q])
+    @event_number = $event_number
   end
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,26 +1,10 @@
 class Venue < ActiveRecord::Base
   validates :name, uniqueness: true
-  attr_reader :event_number
-                # :event,
-  #             :date_time,
-  #             :description,
-  #             :venue_name,
-  #             :venue_url,
-  #             :venue_address
-
+  
   def self.zip(zip)
     events = VenueService.zip_search(zip)
-    @event_number = events[:total_items]
+    $event_number = events[:total_items]
     events = events[:events][:event]
-    events.map do |event|
-      require "pry"; binding.pry
-      event =          event[:title]
-      date_time =      event[:start_time]
-      description =    event[:description]   #(limit to 140 chars)
-      venue_name =     event[:venue_name]
-      venue_url =      event[:venue_url]
-      venue_address =  event[:venue_address]
-    end
   end
 
 end
